28d1eba468c1d5e09c57f327c160a3f565854ff8
LittleBird <31522351+hummingly@users.noreply.github.com>
2019-03-31 18:27:07 +0200

    fix error

--- src/renderer/frontend/components/FileInfo.tsx
+++ src/renderer/frontend/components/FileInfo.tsx
@@ -6,9 +6,9 @@ import { Tag } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import { ClientTag } from '../../entities/Tag';
 
-const formatDate = (d: Date) => (
-  `${d.getUTCFullYear()}-${d.getUTCMonth() + 1}-${d.getUTCDate()} ${d.getUTCHours()}:${d.getUTCMinutes()}`
-);
+const formatDate = (d: Date) =>
+  `${d.getUTCFullYear()}-${d.getUTCMonth() +
+    1}-${d.getUTCDate()} ${d.getUTCHours()}:${d.getUTCMinutes()}`;
 
 const SingleFileInfo = observer(({ file }: { file: ClientFile }) => {
   const [fileStats, setFileStats] = useState<fs.Stats | undefined>(undefined);
@@ -16,16 +16,24 @@ const SingleFileInfo = observer(({ file }: { file: ClientFile }) => {
 
   // Look up file info when component mounts
   useEffect(() => {
-    fs.stat(file.path, (err, stats) => err ? setError(err) : setFileStats(stats));
+    fs.stat(file.path, (err, stats) =>
+      err ? setError(err) : setFileStats(stats),
+    );
   }, []);
 
   // Todo: Would be nice to also add tooltips explaining what these mean (e.g. diff between dimensions & resolution)
   // Or add the units: pixels vs DPI
   const fileInfoList = [
     { key: 'Filename', value: file.path },
-    { key: 'Created', value: fileStats ? formatDate(fileStats.birthtime) : '...' },
+    {
+      key: 'Created',
+      value: fileStats ? formatDate(fileStats.birthtime) : '...',
+    },
     { key: 'Modified', value: fileStats ? formatDate(fileStats.ctime) : '...' },
-    { key: 'Last Opened', value: fileStats ? formatDate(fileStats.atime) : '...' },
+    {
+      key: 'Last Opened',
+      value: fileStats ? formatDate(fileStats.atime) : '...',
+    },
     { key: 'Dimensions', value: '?' },
     { key: 'Resolution', value: '?' },
     { key: 'Color Space', value: '?' },
@@ -33,29 +41,27 @@ const SingleFileInfo = observer(({ file }: { file: ClientFile }) => {
 
   return (
     <>
-      {fileInfoList.map(
-        ({ key, value }, index) => (
-          <div key={`fileInfoIndex-${index}`}>
-            <div className="fileInfoKey">{key}</div>
-            <div className="fileInfoValue">{value}</div>
-          </div>
-        ),
-      )}
+      {fileInfoList.map(({ key, value }, index) => (
+        <div key={`fileInfoIndex-${index}`}>
+          <div className="fileInfoKey">{key}</div>
+          <div className="fileInfoValue">{value}</div>
+        </div>
+      ))}
       <div>
         <div className="fileInfoKey fileInfoKeyMulti">Tags</div>
         <div>
           {file.clientTags.map((t, i) => (
-              <Tag
-                key={`tag-${i}`}
-                onRemove={() => console.log('Remove tag')}
-              >
-                {t.name}
-              </Tag>
-            ))
-          }
+            <Tag key={`tag-${i}`} onRemove={() => console.log('Remove tag')}>
+              {t.name}
+            </Tag>
+          ))}
         </div>
       </div>
-      {error && <p>Error: {error.name} <br /> {error.message}</p>}
+      {error && (
+        <p>
+          Error: {error.name} <br /> {error.message}
+        </p>
+      )}
     </>
   );
 });
@@ -68,7 +74,8 @@ const MultiFileInfo = observer(({ files }: IFileInfoProps) => {
   allTags.forEach((t) => countMap.set(t, (countMap.get(t) || 0) + 1));
 
   // Sort based on count
-  const sortedTags = [...countMap.entries()].sort((a, b) => b[1] - a[1]);
+  // tslint:disable-next-line: newline-per-chained-call
+  const sortedTags = Array.from(countMap.entries()).sort((a, b) => b[1] - a[1]);
 
   return (
     <>
@@ -77,14 +84,10 @@ const MultiFileInfo = observer(({ files }: IFileInfoProps) => {
         <div className="fileInfoKey fileInfoKeyMulti">Tags</div>
         <div>
           {sortedTags.map(([tag, count], i) => (
-              <Tag
-                key={`tag-${i}`}
-                onRemove={() => console.log('Remove tag')}
-              >
-                {tag.name} ({count})
-              </Tag>
-            ))
-          }
+            <Tag key={`tag-${i}`} onRemove={() => console.log('Remove tag')}>
+              {tag.name} ({count})
+            </Tag>
+          ))}
         </div>
       </div>
     </>
@@ -95,14 +98,9 @@ interface IFileInfoProps {
   files: ClientFile[];
 }
 
-const FileInfo = ({
-  files,
-}: IFileInfoProps) => {
-
+const FileInfo = ({ files }: IFileInfoProps) => {
   if (files.length === 0) {
-    return (
-      <br />
-    );
+    return <br />;
   } else if (files.length === 1) {
     return <SingleFileInfo file={files[0]} />;
   } else {
