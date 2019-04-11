dec16dbf51c59848742441ef6af179cdc7ba98b5
Remi van der Laan <R.M.vanderlaan@student.tudelft.nl>
2019-04-11 22:33:05 +0200

    Added query overview at the top of the gallery

--- src/renderer/frontend/components/FileList.tsx
+++ src/renderer/frontend/components/FileList.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import { withRootstore, IRootStoreProp } from '../contexts/StoreContext';
 import Gallery from './Gallery';
 import FileSelectionHeader from './FileSelectionHeader';
+import { Tag, ITagProps } from '@blueprintjs/core';
 
 export interface IFileListProps extends IRootStoreProp {}
 
-const FileList = ({ rootStore: { uiStore, fileStore } }: IFileListProps) => {
-  const removeSelectedFiles = async () => {
-    await fileStore.removeFilesById(uiStore.fileSelection);
-    uiStore.fileSelection.clear();
-  };
+const FileList = ({ rootStore: { uiStore, fileStore, tagStore } }: IFileListProps) => {
+
+  const handleClearFileSelection = useCallback(
+    () => uiStore.fileSelection.clear(),
+    [],
+  );
+
+  const removeSelectedFiles = useCallback(
+    async () => {
+      await fileStore.removeFilesById(uiStore.fileSelection);
+      uiStore.fileSelection.clear();
+    },
+    [],
+  );
+
+  const handleDeselectTag = useCallback(
+    (_, props: ITagProps) => {
+      const clickedTag = tagStore.tagList.find((t) => t.id === props.id);
+      if (clickedTag) {
+        uiStore.deselectTag(clickedTag);
+      }
+    },
+    [],
+  );
 
   const selectionModeOn = uiStore.fileSelection.length > 0;
 
@@ -20,10 +40,23 @@ const FileList = ({ rootStore: { uiStore, fileStore } }: IFileListProps) => {
       { selectionModeOn && (
         <FileSelectionHeader
           numSelectedFiles={uiStore.fileSelection.length}
-          onCancel={() => uiStore.fileSelection.clear()}
+          onCancel={handleClearFileSelection}
           onRemove={removeSelectedFiles}
         />
       )}
+
+      <div id="query-overview">
+        { uiStore.clientTagSelection.map((tag) => (
+          <Tag
+            key={tag.id}
+            id={tag.id}
+            intent="primary"
+            onRemove={handleDeselectTag}
+          >
+            {tag.name}
+          </Tag>),
+        )}
+      </div>
 
       <Gallery />
     </div>
