b627e003c78f1fc97c88957b442684b81e39271a
Remi van der Laan <R.M.vanderlaan@student.tudelft.nl>
2019-06-27 23:26:18 +0200

    Added click handlers to left/right preview window buttons

--- src/renderer/frontend/components/Preview.tsx
+++ src/renderer/frontend/components/Preview.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useCallback } from 'react';
 
 import StoreContext from '../contexts/StoreContext';
 import ErrorBoundary from './ErrorBoundary';
@@ -7,10 +7,18 @@ import { Button, Switch } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 
 const PreviewApp = () => {
-  const { uiStore } = useContext(StoreContext);
+  const { uiStore, fileStore } = useContext(StoreContext);
   const themeClass = uiStore.theme === 'DARK' ? 'bp3-dark' : 'bp3-light';
 
   useEffect(uiStore.viewSlide, []);
+
+  const handleLeftButton = useCallback(
+    () => uiStore.setFirstIndexInView(Math.max(0, uiStore.firstIndexInView - 1)),
+    []);
+
+  const handleRightButton = useCallback(
+    () => uiStore.setFirstIndexInView(Math.min(uiStore.firstIndexInView + 1, fileStore.fileList.length - 1)),
+    [fileStore.fileList.length]);
 
   return (
     <div className={`${themeClass}`} style={{ height: '100%' }}>
@@ -19,17 +27,15 @@ const PreviewApp = () => {
           <section id="outliner-toolbar">
             <Button
               icon="arrow-left"
-              // Todo: Fixme
-              onClick={() => console.log('This will work in the selection-improvements branch')}
+              onClick={handleLeftButton}
               minimal
-              disabled
+              disabled={uiStore.firstIndexInView === 0}
             />
             <Button
               icon="arrow-right"
-              // Todo: Fixme
-              onClick={() => console.log('This will work in the selection-improvements branch')}
+              onClick={handleRightButton}
               minimal
-              disabled
+              disabled={uiStore.firstIndexInView === fileStore.fileList.length - 1}
             />
             <Switch
               label="Overview"
