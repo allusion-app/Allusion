fcaa89b749823b90ff158ddb0cff0d0f05f3875a
Remi van der Laan <R.M.vanderlaan@student.tudelft.nl>
2019-11-16 13:45:15 +0100

    Fixes #90, Improved tag text color for arbitrary background colors

--- src/renderer/frontend/utils.ts
+++ src/renderer/frontend/utils.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 import { thumbnailType } from '../../config';
 
+////////////////////////
+//// Time-out utils ////
+////////////////////////
 export function debounce<F extends (...args: any) => any>(func: F, wait: number = 300): F {
   let timeoutID: number;
 
@@ -38,6 +41,9 @@ export const timeoutPromise = <T>(timeMS: number, promise: Promise<T>): Promise<
   });
 }
 
+///////////////////////////////
+//// Text formatting utils ////
+///////////////////////////////
 export const formatTagCountText = (numTags: number, numCols: number) => {
   const extraTagsText = numTags ? `+${numTags} tag${numTags === 1 ? '' : 's'}` : '';
   const extraColsText = numCols
@@ -66,6 +72,10 @@ export const camelCaseToSpaced = (value: string) => {
   );
 }
 
+
+/////////////////////////
+//// Date/time utils ////
+/////////////////////////
 const DateTimeFormat = new Intl.DateTimeFormat(undefined, {
   timeZone: 'UTC',
   year: 'numeric',
@@ -92,6 +102,9 @@ export const jsDateFormatter = {
   placeholder: 'Choose a date...',
 };
 
+//////////////////////
+//// Color utils /////
+//////////////////////
 export const hexToHSL = (H: string) => {
   // Convert hex to RGB first
   let r = 0;
@@ -143,12 +156,46 @@ export const hexToHSL = (H: string) => {
   return [h, s, l];
 }
 
-export const getClassForBackground = (backHex: string) => {
-  const hsl = hexToHSL(backHex);
-  const [, sat, lum] = hsl;
+/*!
+ * Get the contrasting color for any hex color
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * Derived from work by Brian Suda, https://24ways.org/2010/calculating-color-contrast/
+ * @param  {String} A hexcolor value
+ * @return {String} The contrasting color (black or white)
+ */
+export const getContrast = (hexcolor: string) => {
+	// If a leading # is provided, remove it
+	if (hexcolor.slice(0, 1) === '#') {
+		hexcolor = hexcolor.slice(1);
+	}
 
-  return lum < 50 || (lum < 60 && sat > 75) ? 'color-white' : 'color-black';
+	// If a three-character hexcode, make six-character
+	if (hexcolor.length === 3) {
+		hexcolor = hexcolor.split('').map(function (hex) {
+			return hex + hex;
+		}).join('');
+	}
+
+	// Convert to RGB value
+	const r = parseInt(hexcolor.substr(0,2),16);
+	const g = parseInt(hexcolor.substr(2,2),16);
+	const b = parseInt(hexcolor.substr(4,2),16);
+
+	// Get YIQ ratio
+	const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+	return yiq;
+};
+
+export const getClassForBackground = (backHex: string) => {
+  // const hsl = hexToHSL(backHex);
+  // const [, sat, lum] = hsl;
+  // return lum < 50 || (lum < 60 && sat > 75) ? 'color-white' : 'color-black';
+  return getContrast(backHex) >= 128 ? 'color-black' : 'color-white'
 }
+
+////////////////////
+//// Misc utils ////
+////////////////////
 
 export const hashString = (s: string) => {
   let hash = 0;
