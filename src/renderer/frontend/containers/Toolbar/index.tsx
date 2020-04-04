52542dff951522b562839649ce0fae242dfa98d0
Rombout Versluijs <romboutv@gmail.com>
2020-04-03 23:47:49 -0400

    tweaking tag tree, trying to fix hover mis-alignment

--- src/renderer/frontend/containers/Toolbar/index.tsx
+++ src/renderer/frontend/containers/Toolbar/index.tsx
@@ -96,6 +96,7 @@ const TagFilesPopover = observer(({ disabled, files, uiStore }: ITagFilesPopover
     minimal
     isOpen={uiStore.isToolbarTagSelectorOpen}
     onClose={uiStore.closeToolbarTagSelector}
+    position={"bottom"}
   >
     <Button
       icon={IconSet.TAG}
@@ -261,6 +262,7 @@ const Toolbar = observer(() => {
                 <Button icon={IconSet.FILTER} className="tooltip" data-right={Tooltip.Filter} />
               }
               content={sortMenu}
+              position={"bottom"}
             />
           </ButtonGroup>
         )}
