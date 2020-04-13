e7b7b731ea0f0c4e34e6aabf26e0e9e29b4686b0
LittleBird <31522351+hummingly@users.noreply.github.com>
2020-04-13 14:43:28 +0200

    Replace react-inline-svg with svgr Updating react-inline-svg is impossible as it doesn't support the node.js use case (anymore) because it assumes that sources are fetched linke in a browser.

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,117 +7,117 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 let mainConfig = {
-    mode: 'production',
-    entry: './src/main/main.ts',
-    target: 'electron-main',
-    output: {
-        filename: 'main.bundle.js',
-        path: __dirname + '/dist',
-    },
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    resolve: {
-        extensions: ['.js', '.json', '.ts'],
-    },
-    module: {
-        rules: [
-            {
-                // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader' (except test files)
-                test: /\.(ts)$/,
-                exclude: [/node_modules/, '/src/**/*.test.ts'],
-                use: {
-                    loader: 'ts-loader',
-                },
-            },
-            {
-                test: /\.(jpg|png|ico|icns)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]',
-                },
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader?classPrefix'
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]',
-                },
-            },
-        ],
-    },
+  mode: 'production',
+  entry: './src/main/main.ts',
+  target: 'electron-main',
+  output: {
+    filename: 'main.bundle.js',
+    path: __dirname + '/dist',
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader' (except test files)
+        test: /\.(ts)$/,
+        exclude: [/node_modules/, '/src/**/*.test.ts'],
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(jpg|png|ico|icns)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader?classPrefix',
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+  },
 };
 
 let rendererConfig = {
-    mode: 'production',
-    entry: './src/renderer/renderer.tsx',
-    target: 'electron-renderer',
-    output: {
-        filename: 'renderer.bundle.js',
-        path: __dirname + '/dist',
-    },
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    resolve: {
-        extensions: ['.js', '.json', '.ts', '.tsx'],
-    },
-    module: {
-        rules: [
-            { 
-                test: /\.worker\.ts$/,
-                loader: 'worker-loader',
-                options: {
-                    name: '[name].js',
-                },
-            },
-            {
-                // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'ts-loader',
-                },
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader',
-                    'css-loader?sourceMap',
-                    'sass-loader?sourceMap',
-                ],
-            },
-            {
-                test: /\.(jpg|png|svg|ico|icns)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]',
-                },
-            },
-            {
-                test: /\.(eot|ttf|woff|woff2)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]',
-                },
-            },
-            {
-                test: /\.node$/,
-                use: 'node-loader',
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/renderer/index.html'),
-        }),
+  mode: 'production',
+  entry: './src/renderer/renderer.tsx',
+  target: 'electron-renderer',
+  output: {
+    filename: 'renderer.bundle.js',
+    path: __dirname + '/dist',
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx', '.svg'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.worker\.ts$/,
+        loader: 'worker-loader',
+        options: {
+          name: '[name].js',
+        },
+      },
+      {
+        // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader?sourceMap', 'sass-loader?sourceMap'],
+      },
+      {
+        test: /\.(jpg|png|ico|icns)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/renderer/index.html'),
+    }),
+  ],
 };
 
 module.exports = [mainConfig, rendererConfig];
