d9b6ccd56ef4ce21c25458b3bb3b9991ee1508a9
Little Bird <31522351+hummingly@users.noreply.github.com>
2020-11-04 16:51:46 +0100

    Move slide mode into its own module

--- src/renderer/frontend/containers/ContentView/Gallery.tsx
+++ src/renderer/frontend/containers/ContentView/Gallery.tsx
@@ -2,13 +2,13 @@ import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react'
 import { FixedSizeList } from 'react-window';
 import { observer } from 'mobx-react-lite';
 
-import { GridCell, ListCell, MissingImageFallback } from './GalleryItem';
+import { GridCell, ListCell } from './GalleryItem';
 import { ClientFile } from '../../../entities/File';
 import { IconSet } from 'components';
 import { MenuItem } from 'components/menu';
 import { throttle } from '../../utils';
 import { shell } from 'electron';
-import ZoomableImage from './ZoomableImage';
+import SlideMode from './SlideMode';
 import { RendererMessenger } from 'src/Messaging';
 import { DnDAttribute, DnDType } from '../Outliner/TagsPanel/DnD';
 import UiStore, { ViewMethod } from '../../stores/UiStore';
@@ -86,7 +86,7 @@ const Layout = ({
   }, [fileStore, handleFileSelect]);
 
   if (uiStore.isSlideMode) {
-    return <SlideGallery contentRect={contentRect} uiStore={uiStore} fileStore={fileStore} />;
+    return <SlideMode contentRect={contentRect} uiStore={uiStore} fileStore={fileStore} />;
   }
   switch (uiStore.method) {
     case ViewMethod.Grid:
@@ -572,116 +572,6 @@ const GridRow = observer((props: IGridRow) => {
 //     </div>
 //   );
 // });
-
-const SlideGallery = observer((props: { contentRect: Dimension } & UiStoreProp & FileStoreProp) => {
-  const { contentRect, uiStore, fileStore } = props;
-  const { fileList } = fileStore;
-  // Go to the first selected image on load
-  useEffect(() => {
-    if (uiStore.firstSelectedFile !== undefined) {
-      uiStore.setFirstItem(fileStore.getIndex(uiStore.firstSelectedFile.id));
-    }
-  }, [fileStore, uiStore]);
-
-  // Go back to previous view when pressing the back button (mouse button 5)
-  useEffect(() => {
-    // Push a dummy state, so that a pop-state event can be activated
-    history.pushState(null, document.title, location.href);
-    const popStateHandler = uiStore.disableSlideMode;
-    window.addEventListener('popstate', popStateHandler);
-    return () => window.removeEventListener('popstate', popStateHandler);
-  }, [uiStore.disableSlideMode]);
-
-  // Automatically select the active image, so it is shown in the inspector
-  useEffect(() => {
-    if (uiStore.firstItem < fileList.length) {
-      uiStore.selectFile(fileList[uiStore.firstItem], true);
-    }
-  }, [fileList, uiStore]);
-
-  const decrImgIndex = useCallback(() => uiStore.setFirstItem(Math.max(0, uiStore.firstItem - 1)), [
-    uiStore,
-  ]);
-  const incrImgIndex = useCallback(
-    () => uiStore.setFirstItem(Math.min(uiStore.firstItem + 1, fileList.length - 1)),
-    [uiStore, fileList.length],
-  );
-
-  // Detect left/right arrow keys to scroll between images
-  const handleUserKeyPress = useCallback(
-    (event: KeyboardEvent) => {
-      if (event.key === 'ArrowLeft') {
-        decrImgIndex();
-      } else if (event.key === 'ArrowRight') {
-        incrImgIndex();
-      } else if (event.key === 'Escape' || event.key === 'Backspace') {
-        uiStore.disableSlideMode();
-      }
-    },
-    [incrImgIndex, decrImgIndex, uiStore],
-  );
-
-  // Detect scroll wheel to scroll between images
-  const handleUserWheel = useCallback(
-    (event: WheelEvent) => {
-      if (event.ctrlKey) {
-        return;
-      }
-      event.preventDefault();
-
-      if (event.deltaY > 0) {
-        decrImgIndex();
-      } else if (event.deltaY < 0) {
-        incrImgIndex();
-      }
-    },
-    [incrImgIndex, decrImgIndex],
-  );
-
-  // Set up event listeners
-  useEffect(() => {
-    window.addEventListener('keydown', handleUserKeyPress);
-    // window.addEventListener('wheel', handleUserWheel, { passive: false });
-    return () => {
-      window.removeEventListener('keydown', handleUserKeyPress);
-      // window.removeEventListener('wheel', handleUserWheel);
-    };
-  }, [handleUserKeyPress, handleUserWheel]);
-
-  // Preload next and previous image for better UX
-  useEffect(() => {
-    if (uiStore.firstItem + 1 < fileList.length) {
-      const nextImg = new Image();
-      nextImg.src = fileList[uiStore.firstItem + 1].absolutePath;
-    }
-    if (uiStore.firstItem - 1 >= 0) {
-      const prevImg = new Image();
-      prevImg.src = fileList[uiStore.firstItem - 1].absolutePath;
-    }
-  }, [fileList, uiStore.firstItem]);
-
-  if (uiStore.firstItem >= fileList.length) {
-    return <p>No files available</p>;
-  }
-
-  const file = fileList[uiStore.firstItem];
-
-  return file.isBroken ? (
-    <MissingImageFallback
-      style={{
-        width: `${contentRect.width}px`,
-        height: `${contentRect.height}px`,
-      }}
-    />
-  ) : (
-    <ZoomableImage
-      src={file.absolutePath}
-      contentRect={contentRect}
-      prevImage={uiStore.firstItem - 1 >= 0 ? decrImgIndex : undefined}
-      nextImage={uiStore.firstItem + 1 < fileList.length ? incrImgIndex : undefined}
-    />
-  );
-});
 
 const MissingFileMenuItems = observer(({ uiStore, fileStore }: UiStoreProp & FileStoreProp) => (
   <>
--- src/renderer/frontend/containers/ContentView/SlideMode.tsx
+++ src/renderer/frontend/containers/ContentView/SlideMode.tsx
@@ -1,7 +1,127 @@
-import React, { useCallback } from 'react';
-
+import React, { useCallback, useEffect } from 'react';
+import { observer } from 'mobx-react-lite';
 import PinchZoomPan from 'react-responsive-pinch-zoom-pan';
+
 import { IconSet } from 'components';
+import { MissingImageFallback } from './GalleryItem';
+import UiStore from '../../stores/UiStore';
+import FileStore from '../../stores/FileStore';
+
+interface SlideModeProps {
+  contentRect: { width: number; height: number };
+  uiStore: UiStore;
+  fileStore: FileStore;
+}
+
+const SlideMode = (props: SlideModeProps) => {
+  const { contentRect, uiStore, fileStore } = props;
+  const { fileList } = fileStore;
+  // Go to the first selected image on load
+  useEffect(() => {
+    if (uiStore.firstSelectedFile !== undefined) {
+      uiStore.setFirstItem(fileStore.getIndex(uiStore.firstSelectedFile.id));
+    }
+  }, [fileStore, uiStore]);
+
+  // Go back to previous view when pressing the back button (mouse button 5)
+  useEffect(() => {
+    // Push a dummy state, so that a pop-state event can be activated
+    history.pushState(null, document.title, location.href);
+    const popStateHandler = uiStore.disableSlideMode;
+    window.addEventListener('popstate', popStateHandler);
+    return () => window.removeEventListener('popstate', popStateHandler);
+  }, [uiStore.disableSlideMode]);
+
+  // Automatically select the active image, so it is shown in the inspector
+  useEffect(() => {
+    if (uiStore.firstItem < fileList.length) {
+      uiStore.selectFile(fileList[uiStore.firstItem], true);
+    }
+  }, [fileList, uiStore]);
+
+  const decrImgIndex = useCallback(() => uiStore.setFirstItem(Math.max(0, uiStore.firstItem - 1)), [
+    uiStore,
+  ]);
+  const incrImgIndex = useCallback(
+    () => uiStore.setFirstItem(Math.min(uiStore.firstItem + 1, fileList.length - 1)),
+    [uiStore, fileList.length],
+  );
+
+  // Detect left/right arrow keys to scroll between images
+  const handleUserKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'ArrowLeft') {
+        decrImgIndex();
+      } else if (event.key === 'ArrowRight') {
+        incrImgIndex();
+      } else if (event.key === 'Escape' || event.key === 'Backspace') {
+        uiStore.disableSlideMode();
+      }
+    },
+    [incrImgIndex, decrImgIndex, uiStore],
+  );
+
+  // Detect scroll wheel to scroll between images
+  const handleUserWheel = useCallback(
+    (event: WheelEvent) => {
+      if (event.ctrlKey) {
+        return;
+      }
+      event.preventDefault();
+
+      if (event.deltaY > 0) {
+        decrImgIndex();
+      } else if (event.deltaY < 0) {
+        incrImgIndex();
+      }
+    },
+    [incrImgIndex, decrImgIndex],
+  );
+
+  // Set up event listeners
+  useEffect(() => {
+    window.addEventListener('keydown', handleUserKeyPress);
+    // window.addEventListener('wheel', handleUserWheel, { passive: false });
+    return () => {
+      window.removeEventListener('keydown', handleUserKeyPress);
+      // window.removeEventListener('wheel', handleUserWheel);
+    };
+  }, [handleUserKeyPress, handleUserWheel]);
+
+  // Preload next and previous image for better UX
+  useEffect(() => {
+    if (uiStore.firstItem + 1 < fileList.length) {
+      const nextImg = new Image();
+      nextImg.src = fileList[uiStore.firstItem + 1].absolutePath;
+    }
+    if (uiStore.firstItem - 1 >= 0) {
+      const prevImg = new Image();
+      prevImg.src = fileList[uiStore.firstItem - 1].absolutePath;
+    }
+  }, [fileList, uiStore.firstItem]);
+
+  if (uiStore.firstItem >= fileList.length) {
+    return <p>No files available</p>;
+  }
+
+  const file = fileList[uiStore.firstItem];
+
+  return file.isBroken ? (
+    <MissingImageFallback
+      style={{
+        width: `${contentRect.width}px`,
+        height: `${contentRect.height}px`,
+      }}
+    />
+  ) : (
+    <ZoomableImage
+      src={file.absolutePath}
+      contentRect={contentRect}
+      prevImage={uiStore.firstItem - 1 >= 0 ? decrImgIndex : undefined}
+      nextImage={uiStore.firstItem + 1 < fileList.length ? incrImgIndex : undefined}
+    />
+  );
+};
 
 interface IZoomableImageProps {
   src: string;
@@ -277,4 +397,4 @@ const ZoomableImage = ({ src, contentRect, prevImage, nextImage }: IZoomableImag
 //   );
 // };
 
-export default ZoomableImage;
+export default observer(SlideMode);
