bd5649dc34d1fb7f20cc1b7bd8eb178cc1e5ece1
Little Bird <31522351+hummingly@users.noreply.github.com>
2020-12-21 17:23:14 +0100

    Refactor placholder components * Logic and rendering is separated which should result in smaller diffs in the future. * Each branch is now a single component, so this should also lead to less churn.

--- src/renderer/frontend/containers/ContentView/Placeholder.tsx
+++ src/renderer/frontend/containers/ContentView/Placeholder.tsx
@@ -1,42 +1,60 @@
 import React, { useContext } from 'react';
-import StoreContext from '../../contexts/StoreContext';
-
-import { IconSet, Button, ButtonGroup } from 'components';
 import { observer } from 'mobx-react-lite';
 
+import StoreContext from '../../contexts/StoreContext';
+
 const Placeholder = observer(() => {
-  const { fileStore, uiStore, tagStore } = useContext(StoreContext);
-  let icon;
-  let title;
-  let description;
-  let action;
+  const { fileStore, tagStore, uiStore } = useContext(StoreContext);
 
   if (fileStore.showsAllContent && tagStore.tagList.length === 1) {
     // No tags exist, and no images added: Assuming it's a new user -> Show a welcome screen
-    icon = IconSet.LOGO;
-    title = 'Welcome to Allusion';
-    description = (
-      <>
-        <p>
-          Allusion is a tool designed to help you organize your Visual Library, so you can easily
-          find what you need throughout your creative process.
-        </p>
-        <p>Allusion needs to know where to find your images. Click below to get started.</p>
-      </>
-    );
-    action = (
-      <Button text="Getting Started Guide" onClick={() => window.alert('TODO: Get started')} />
-    );
+    return <Welcome />;
   } else if (fileStore.showsAllContent) {
-    icon = IconSet.MEDIA;
-    title = 'No images';
-    description = <p>Images can be added from the outliner</p>;
-    action = <Button onClick={uiStore.toggleOutliner} text="Toggle outliner" styling="outlined" />;
+    return <NoContentFound uiStore={uiStore} />;
   } else if (fileStore.showsQueryContent) {
-    description = <p>Try searching for something else.</p>;
-    icon = IconSet.SEARCH;
-    title = 'No images found';
-    action = (
+    return <NoQueryContent fileStore={fileStore} />;
+  } else if (fileStore.showsUntaggedContent) {
+    return <NoUntaggedContent fileStore={fileStore} />;
+  } else if (fileStore.showsMissingContent) {
+    return <NoMissingContent fileStore={fileStore} />;
+  } else {
+    return <BugReport />;
+  }
+});
+
+export default Placeholder;
+
+import FileStore from '../../stores/FileStore';
+import UiStore from '../../stores/UiStore';
+
+import { IconSet, Button, ButtonGroup } from 'components';
+
+const Welcome = () => {
+  return (
+    <ContentPlaceholder title="Welcome to Allusion" icon={IconSet.LOGO}>
+      <p>
+        Allusion is a tool designed to help you organize your Visual Library, so you can easily find
+        what you need throughout your creative process.
+      </p>
+      <p>Allusion needs to know where to find your images. Click below to get started.</p>
+      <Button text="Getting Started Guide" onClick={() => window.alert('TODO: Get started')} />
+    </ContentPlaceholder>
+  );
+};
+
+const NoContentFound = ({ uiStore }: { uiStore: UiStore }) => {
+  return (
+    <ContentPlaceholder title="No images" icon={IconSet.MEDIA}>
+      <p>Images can be added from the outliner</p>
+      <Button onClick={uiStore.toggleOutliner} text="Toggle outliner" styling="outlined" />
+    </ContentPlaceholder>
+  );
+};
+
+const NoQueryContent = ({ fileStore }: { fileStore: FileStore }) => {
+  return (
+    <ContentPlaceholder title="No images found" icon={IconSet.SEARCH}>
+      <p>Try searching for something else.</p>
       <ButtonGroup>
         <Button
           text="All images"
@@ -51,24 +69,28 @@ const Placeholder = observer(() => {
           styling="outlined"
         />
       </ButtonGroup>
-    );
-  } else if (fileStore.showsUntaggedContent) {
-    icon = IconSet.TAG;
-    description = <p>All images have been tagged. Nice work!</p>;
-    title = 'No untagged images';
-    action = (
+    </ContentPlaceholder>
+  );
+};
+
+const NoUntaggedContent = ({ fileStore }: { fileStore: FileStore }) => {
+  return (
+    <ContentPlaceholder title="No untagged images" icon={IconSet.TAG}>
+      <p>All images have been tagged. Nice work!</p>
       <Button
         text="All Images"
         icon={IconSet.MEDIA}
         onClick={fileStore.fetchAllFiles}
         styling="outlined"
       />
-    );
-  } else if (fileStore.showsMissingContent) {
-    icon = IconSet.WARNING_BROKEN_LINK;
-    description = <p>Try searching for something else.</p>;
-    title = 'No missing images';
-    action = (
+    </ContentPlaceholder>
+  );
+};
+
+const NoMissingContent = ({ fileStore }: { fileStore: FileStore }) => {
+  return (
+    <ContentPlaceholder title="No missing images" icon={IconSet.WARNING_BROKEN_LINK}>
+      <p>Try searching for something else.</p>
       <ButtonGroup>
         <Button
           text="All images"
@@ -83,17 +105,30 @@ const Placeholder = observer(() => {
           styling="outlined"
         />
       </ButtonGroup>
-    );
-  }
+    </ContentPlaceholder>
+  );
+};
 
+const BugReport = () => {
+  return (
+    <ContentPlaceholder title="You encountered a bug!" icon={IconSet.WARNING_FILL}>
+      <p>Please report this bug to the maintainers!</p>
+    </ContentPlaceholder>
+  );
+};
+
+interface IContentPlaceholder {
+  icon: JSX.Element;
+  title: string;
+  children: React.ReactNode | React.ReactNodeArray;
+}
+
+const ContentPlaceholder = (props: IContentPlaceholder) => {
   return (
     <div id="content-placeholder">
-      <span className="custom-icon-128">{icon}</span>
-      <h2>{title}</h2>
-      {description}
-      {action}
+      <span className="custom-icon-128">{props.icon}</span>
+      <h2>{props.title}</h2>
+      {props.children}
     </div>
   );
-});
-
-export default Placeholder;
+};
