5c67df6783433b622f1dd5f6bc4ed43775ea7278
Little Bird <31522351+hummingly@users.noreply.github.com>
2020-12-26 22:14:18 +0100

    Remove slide effect from inspector and refactor

--- src/frontend/containers/Inspector/index.tsx
+++ src/frontend/containers/Inspector/index.tsx
@@ -1,115 +1,62 @@
-import React, { useContext, useState, useCallback, useMemo, ReactNode } from 'react';
+import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
 
-import { clamp } from '../../utils';
-
 import StoreContext from '../../contexts/StoreContext';
-
 import FileTags from '../../components/FileTag';
 import ImageInfo from '../../components/ImageInfo';
-import { Slide } from '../../components/Transition';
-
 import { MissingImageFallback } from '../ContentView/GalleryItem';
-
-const Carousel = observer(() => {
-  const { uiStore } = useContext(StoreContext);
-  const items = uiStore.fileSelection;
-  // NOTE: maxItems is coupled to the CSS! Max is 10 atm (see inspector.scss)
-  const maxItems = 7;
-  const [scrollIndex, setScrollIndex] = useState(0);
-
-  // Add some padding items so that you can scroll the last item to the front
-  const paddedItems = useMemo(() => {
-    const padding = new Array(maxItems - 1);
-    setScrollIndex(items.size - 1);
-    return padding.concat(Array.from(items));
-  }, [items]);
-
-  const handleWheel = useCallback(
-    (e: React.WheelEvent) => {
-      const delta = e.deltaY > 0 ? -1 : 1;
-      setScrollIndex((v) => clamp(v + delta, 0, paddedItems.length - 1));
-    },
-    [paddedItems.length],
-  );
-
-  return (
-    <div
-      role="region"
-      className="carousel"
-      aria-roledescription="carousel"
-      aria-label="Gallery Selection"
-      aria-live="polite"
-      onWheel={handleWheel}
-    >
-      {/* Show a stack of the first N images (or fewer) */}
-      {paddedItems.slice(scrollIndex, scrollIndex + maxItems).map((file, index) =>
-        !file ? null : (
-          <div
-            key={file.id}
-            // TODO: Could add in and out transition, but you'd also need to know the scroll direction for that
-            className={`carousel-slide child-${index}`}
-            role="group"
-            aria-roledescription="slide"
-            aria-label={`${scrollIndex + index + 1} of ${items.size}`}
-          >
-            {/* TODO: Thumbnail path is not always resolved atm, working on that in another branch */}
-            <img
-              src={file.thumbnailPath}
-              onClick={() => setScrollIndex(scrollIndex - maxItems + 1 + index)}
-            />
-          </div>
-        ),
-      )}
-    </div>
-  );
-});
+import Carousel from './Carousel';
 
 const Inspector = observer(() => {
   const { uiStore } = useContext(StoreContext);
-  const selectedFiles = uiStore.fileSelection;
+  const first = uiStore.firstSelectedFile;
 
-  if (selectedFiles.size === 0) {
+  if (first === undefined) {
     return (
-      <Slide element="aside" id="inspector" open={uiStore.isInspectorOpen} unmountOnExit>
-        <section>
-          <header>
-            <h2>No image selected</h2>
-          </header>
-        </section>
-      </Slide>
+      <Panel isOpen={uiStore.isInspectorOpen}>
+        <Placeholder />
+      </Panel>
     );
   }
 
-  let selectionPreview: ReactNode;
-  let information: ReactNode;
-
-  if (selectedFiles.size === 1) {
-    const first = uiStore.firstSelectedFile;
-    if (first === undefined) {
-      selectionPreview = <MissingImageFallback />;
-      information = 'The selected file cannot be found. Please check if the given file exists.';
-    } else {
-      selectionPreview = first.isBroken ? (
-        <MissingImageFallback />
-      ) : (
-        <img
-          src={first.absolutePath}
-          style={{ cursor: uiStore.isSlideMode ? undefined : 'zoom-in' }}
-          onClick={uiStore.enableSlideMode}
-        />
-      );
-      information = <ImageInfo file={first} />;
-    }
-  } else {
-    // Stack effects: https://tympanus.net/codrops/2014/03/05/simple-stack-effects/
-    selectionPreview = <Carousel />;
-    information = `Selected ${uiStore.fileSelection.size} files`;
-  }
-
   return (
-    <Slide element="aside" id="inspector" open={uiStore.isInspectorOpen} unmountOnExit>
-      <div className="inspector-preview">{selectionPreview}</div>
+    <Panel isOpen={uiStore.isInspectorOpen}>
+      {uiStore.fileSelection.size === 1 ? (
+        <Content
+          preview={
+            first.isBroken ? (
+              <MissingImageFallback />
+            ) : (
+              <img
+                src={first.absolutePath}
+                style={{ cursor: uiStore.isSlideMode ? undefined : 'zoom-in' }}
+                onClick={uiStore.enableSlideMode}
+              />
+            )
+          }
+          information={<ImageInfo file={first} />}
+        />
+      ) : (
+        <Content
+          preview={<Carousel uiStore={uiStore} />}
+          information={`Selected ${uiStore.fileSelection.size} files`}
+        />
+      )}
+    </Panel>
+  );
+});
+
+export default Inspector;
+
+interface IContent {
+  preview: React.ReactNode;
+  information: React.ReactNode;
+}
+
+const Content = ({ preview, information }: IContent) => {
+  return (
+    <>
+      <div className="inspector-preview">{preview}</div>
       <section>
         <header>
           <h2>Information</h2>
@@ -122,8 +69,24 @@ const Inspector = observer(() => {
         </header>
         <FileTags />
       </section>
-    </Slide>
+    </>
   );
-});
+};
 
-export default Inspector;
+const Placeholder = () => {
+  return (
+    <section>
+      <header>
+        <h2>No image selected</h2>
+      </header>
+    </section>
+  );
+};
+
+const Panel = ({ isOpen, children }: { isOpen: boolean; children: React.ReactNode }) => {
+  return (
+    <aside id="inspector" style={isOpen ? undefined : { width: '0' }}>
+      {isOpen ? children : null}
+    </aside>
+  );
+};
