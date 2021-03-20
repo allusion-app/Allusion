8e9d7c02edff4ac0998d59806f1e297977766f03
Remi van der Laan <rrm-remi@hotmail.com>
2021-03-20 17:32:05 +0100

    List view overhaul (#254)
    * Implemented MVP for new list view
    Still todo: DnD on entire row, style tweaking
    
    * more styles
    
    * Added DnD events to list view + some tweaks
    
    * Fix variable
    
    Co-authored-by: Little Bird <31522351+hummingly@users.noreply.github.com>

--- src/frontend/utils.ts
+++ src/frontend/utils.ts
@@ -194,6 +194,25 @@ export const camelCaseToSpaced = (value: string) => {
   );
 };
 
+export const ellipsize = (
+  value: string,
+  maxLength = 80,
+  type: 'start' | 'middle' | 'end' = 'start',
+) => {
+  if (value.length <= maxLength) {
+    return value;
+  }
+  switch (type) {
+    case 'end':
+      return value.substr(maxLength) + '…';
+    case 'middle':
+      return value.substr(0, maxLength / 2) + '…' + value.substr(value.length - maxLength / 2);
+    case 'start':
+    default:
+      return '…' + value.substr(value.length - maxLength);
+  }
+};
+
 /////////////////////////
 //// Date/time utils ////
 /////////////////////////
@@ -356,3 +375,33 @@ export const clamp = (value: number, min: number, max: number): number => {
     return value;
   }
 };
+
+/**
+ * Format bytes as human-readable text.
+ * From: https://stackoverflow.com/a/14919494/2350481
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ * @return Formatted string.
+ */
+export function humanFileSize(bytes: number, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B';
+  }
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+  return bytes.toFixed(dp) + ' ' + units[u];
+}
