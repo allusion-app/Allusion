f0446c629540e8441210b03b870109b9d7e11795
Remi van der Laan <R.M.vanderlaan@outlook.com>
2021-04-11 14:25:09 +0200

    Fix for inspector tags: didn't show the tags for the current image after navigating left/right

--- src/frontend/components/FileTag.tsx
+++ src/frontend/components/FileTag.tsx
@@ -1,29 +1,16 @@
-import React, { useContext } from 'react';
-import { action, ObservableSet } from 'mobx';
 import { observer } from 'mobx-react-lite';
-
+import React, { useContext } from 'react';
 import { ClientFile } from 'src/entities/File';
-import { ClientTag } from 'src/entities/Tag';
-
-import StoreContext from '../contexts/StoreContext';
-import TagStore from '../stores/TagStore';
-import UiStore from '../stores/UiStore';
-
-import { MultiTagSelector } from './MultiTagSelector';
 import { IconSet } from 'widgets/Icons';
+import StoreContext from '../contexts/StoreContext';
+import { MultiTagSelector } from './MultiTagSelector';
 
 interface IFileTagProp {
-  tagStore: TagStore;
-  uiStore: UiStore;
+  file: ClientFile;
 }
 
-const Single = observer(({ tagStore, uiStore }: IFileTagProp) => {
-  const file = uiStore.firstSelectedFile;
-
-  if (file === undefined) {
-    throw new Error('BUG: No file was selected. A condition was not checked.');
-  }
-
+const FileTags = observer(({ file }: IFileTagProp) => {
+  const { tagStore } = useContext(StoreContext);
   const handleCreate = async (name: string) =>
     file.addTag(await tagStore.create(tagStore.root, name));
 
@@ -47,59 +34,4 @@ const Single = observer(({ tagStore, uiStore }: IFileTagProp) => {
   );
 });
 
-const Multi = observer(({ tagStore, uiStore: { fileSelection: files } }: IFileTagProp) => {
-  const { counter, sortedTags } = countFileTags(files);
-
-  const tagLabel = action((tag: ClientTag) => `${tag.name} (${counter.get(tag)})`);
-
-  const handleCreate = (name: string) => tagStore.create(tagStore.root, name);
-
-  return (
-    <MultiTagSelector
-      selection={sortedTags}
-      onClear={action(() => files.forEach((f) => f.clearTags()))}
-      onDeselect={action((tag) => files.forEach((f) => f.removeTag(tag)))}
-      onSelect={action((tag) => files.forEach((f) => f.addTag(tag)))}
-      tagLabel={tagLabel}
-      extraOptions={[
-        {
-          id: 'create',
-          icon: IconSet.TAG_ADD,
-          label: (input: string) => `Create tag "${input}"`,
-          action: handleCreate,
-          resetQueryOnAction: true,
-          onlyShowWithoutSuggestions: true,
-        },
-      ]}
-    />
-  );
-});
-
-const FileTags = observer(() => {
-  const { uiStore, tagStore } = useContext(StoreContext);
-  if (uiStore.fileSelection.size === 1) {
-    return <Single tagStore={tagStore} uiStore={uiStore} />;
-  } else {
-    return <Multi tagStore={tagStore} uiStore={uiStore} />;
-  }
-});
-
 export default FileTags;
-
-export function countFileTags(files: ObservableSet<ClientFile>) {
-  // Count how often tags are used
-  const counter = new Map<ClientTag, number>();
-  for (const file of files) {
-    for (const tag of file.tags) {
-      const count = counter.get(tag);
-      counter.set(tag, count !== undefined ? count + 1 : 1);
-    }
-  }
-
-  const sortedTags = Array.from(counter.entries())
-    // Sort based on count
-    .sort((a, b) => b[1] - a[1])
-    .map((pair) => pair[0]);
-
-  return { counter, sortedTags };
-}
