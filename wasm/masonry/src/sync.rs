0254f11aa3c8d12e8cfe04c89e3280335370a4c3
Little Bird <31522351+hummingly@users.noreply.github.com>
2021-04-18 21:39:50 +0200

    More type safety

--- wasm/masonry/src/sync.rs
+++ wasm/masonry/src/sync.rs
@@ -1,13 +1,13 @@
 use alloc::boxed::Box;
-use core::sync::atomic::{AtomicI32, AtomicU32, Ordering};
+use core::sync::atomic::{AtomicI32, AtomicPtr, AtomicU32, Ordering};
 
 use wasm_bindgen::prelude::*;
 
 use crate::data::{Computation, MasonryType};
 
 static LOCK: AtomicI32 = AtomicI32::new(0);
-static INPUT: AtomicU32 = AtomicU32::new(0); // computation: *mut Computation
-static OUTPUT: AtomicU32 = AtomicU32::new(0); // container_height: u32
+static INPUT: AtomicPtr<Computation> = AtomicPtr::new(core::ptr::null_mut());
+static OUTPUT: AtomicU32 = AtomicU32::new(0);
 
 const LOCKED: i32 = 0;
 const UNLOCKED: i32 = 1;
@@ -22,8 +22,18 @@ const UNLOCKED: i32 = 1;
 pub fn compute() {
     atomic_wait32(&LOCK, LOCKED, -1);
     let computation_ptr = INPUT.load(Ordering::Acquire);
-    let container_height = execute(computation_ptr);
+    let container_height = {
+        if computation_ptr.is_null() {
+            0
+        } else {
+            // SAFETY: The send [`Computation`] is send from the main thread that created that this web
+            // worker. On creation the same memory was used.
+            let computation = unsafe { Box::from_raw(computation_ptr as *mut Computation) };
+            execute(*computation)
+        }
+    };
     OUTPUT.store(container_height, Ordering::Release);
+    INPUT.store(core::ptr::null_mut(), Ordering::Release);
     LOCK.store(LOCKED, Ordering::Release);
 }
 
@@ -32,7 +42,7 @@ pub fn compute() {
 // memory. As soon as the memory at index 0 becomes 1 the web worker thread will stop waiting
 // (see [`create_web_worker`]);
 pub fn send_computation(computation: *mut Computation) {
-    INPUT.store(computation as u32, Ordering::Release);
+    INPUT.store(computation, Ordering::Release);
     LOCK.store(UNLOCKED, Ordering::Release);
     atomic_notify(&LOCK, 1);
 }
@@ -41,17 +51,13 @@ pub fn read_result() -> JsValue {
     JsValue::from(OUTPUT.load(Ordering::Acquire))
 }
 
-fn execute(computation_ptr: u32) -> u32 {
+fn execute(computation: Computation) -> u32 {
     let (width, config, layout) = {
-        // SAFETY: The send [`Computation`] is send from the main thread that created that this web
-        // worker. On creation the same memory was used.
-        let computation = unsafe { Box::from_raw(computation_ptr as *mut Computation) };
         // SAFETY: Never use core::ptr::read. The returned value will be an owned value, which means
         // its destructor will be run at the end of the function. This will lead to a double free.
         // Instead we only get a mutable reference and have to depend on the user to `await` every
         // `Promise` returned from `MasonryWorker::compute`.
-        let layout = unsafe { computation.layout_ptr.as_mut() };
-        match layout {
+        match unsafe { computation.layout_ptr.as_mut() } {
             Some(layout) => (computation.width, computation.config, layout),
             None => return 0,
         }
