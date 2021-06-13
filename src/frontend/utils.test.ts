a87a8311dd23c5ae41dee2b50c494a4bb24e6b34
Remi van der Laan <rrm-remi@hotmail.com>
2021-06-13 13:38:32 +0200

    Test fix: backslash file separator not supported on linux (where tests run on CI)

--- src/frontend/utils.test.ts
+++ src/frontend/utils.test.ts
@@ -3,7 +3,7 @@ import { encodeFilePath } from './utils';
 describe('utils', () => {
   describe('encodeFilePath', () => {
     it('should encode simple paths correctly (unaffected)', () => {
-      expect(encodeFilePath('C:\\test\\test.jpg')).toEqual('C:\\test\\test.jpg');
+      expect(encodeFilePath('C:/test/test.jpg')).toEqual('C:/test/test.jpg');
     });
     it('should encode weird paths correctly (unaffected)', () => {
       expect(encodeFilePath('C:/Images/https_%2F%2Fcdn/my-image.jpg')).toEqual(
@@ -11,10 +11,10 @@ describe('utils', () => {
       );
     });
     it('should encode weird filenames correctly', () => {
-      expect(encodeFilePath('C:\\test\\test space.jpg')).toEqual('C:\\test\\test%20space.jpg');
+      expect(encodeFilePath('C:/test/test space.jpg')).toEqual('C:/test/test%20space.jpg');
     });
     it('should encode paths with params correctly', () => {
-      expect(encodeFilePath('C:\\test\\test.jpg?v=1')).toEqual('C:\\test\\test.jpg?v=1');
+      expect(encodeFilePath('C:/test/test.jpg?v=1')).toEqual('C:/test/test.jpg?v=1');
     });
   });
 });
