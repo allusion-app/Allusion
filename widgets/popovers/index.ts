f6bc8924201e7a8e66ba1a75c116eaae4b90f60c
Little Bird <31522351+hummingly@users.noreply.github.com>
2021-08-18 19:53:09 +0200

    Split files

--- widgets/popovers/index.ts
+++ widgets/popovers/index.ts
@@ -2,6 +2,7 @@ import './popover.scss';
 
 import { Flyout } from './Flyout';
 import { TooltipLayer } from './Tooltip';
-import { Alert, Dialog, DialogButton } from './Dialog';
+import { Dialog } from './Dialog';
+import { Alert, DialogButton } from './Alert';
 
 export { Alert, Dialog, DialogButton, Flyout, TooltipLayer };
