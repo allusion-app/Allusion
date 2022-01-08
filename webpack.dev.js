471898a8c0dbab51ce25d8afa7c29495c7a7c559
hummingly <31522351+hummingly@users.noreply.github.com>
2022-01-08 18:07:19 +0100

    Update asset relate dev deps

--- webpack.dev.js
+++ webpack.dev.js
@@ -82,7 +82,7 @@ let rendererConfig = {
         exclude: /\.module\.scss$/,
         use: [
           'style-loader',
-          'css-loader?sourceMap',
+          { loader: 'css-loader', options: { sourceMap: true } },
           { loader: 'sass-loader', options: { sourceMap: true } },
         ],
       },
