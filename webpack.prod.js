148d70f5124039777fc03409074a66ac3b1b0f4b
Remi van der Laan <rrm-remi@hotmail.com>
2022-03-07 21:46:46 +0100

    Re-added node-loader dependency to fix Mac release hopefully

--- webpack.prod.js
+++ webpack.prod.js
@@ -122,6 +122,10 @@ let rendererConfig = {
         type: 'asset/resource',
       },
       {
+        test: /.node$/,
+        loader: 'node-loader',
+      },
+      {
         test: /\.js$/,
         resourceQuery: /file/,
         type: 'asset/resource',
