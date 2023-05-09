1ccfdc40dfb734aca2d62ff8115b62b69193f7b3
unknown <132866503+pianissi@users.noreply.github.com>
2023-05-09 21:09:40 +1000

    Pause videos when slide mode is active

--- src/frontend/containers/ContentView/GalleryItem.tsx
+++ src/frontend/containers/ContentView/GalleryItem.tsx
@@ -20,6 +20,7 @@ interface ItemProps {
   forceNoThumbnail?: boolean;
   hovered?: boolean;
   galleryVideoPlaybackMode?: GalleryVideoPlaybackMode;
+  isSlideMode?: boolean;
 }
 
 interface MasonryItemProps extends ItemProps {
@@ -70,7 +71,14 @@ export const MasonryCell = observer(
               : (e: React.MouseEvent): void => {}
           }
         >
-          <Thumbnail mounted={mounted} file={file} forceNoThumbnail={forceNoThumbnail} hovered={isHovered} galleryVideoPlaybackMode={uiStore.galleryVideoPlaybackMode}/>
+          <Thumbnail
+            mounted={mounted}
+            file={file}
+            forceNoThumbnail={forceNoThumbnail}
+            hovered={isHovered}
+            galleryVideoPlaybackMode={uiStore.galleryVideoPlaybackMode}
+            isSlideMode={uiStore.isSlideMode}
+          />
         </div>
         {file.isBroken === true && !fileStore.showsMissingContent && (
           <IconButton
@@ -108,134 +116,154 @@ export const MasonryCell = observer(
 
 // TODO: When a filename contains https://x/y/z.abc?323 etc., it can't be found
 // e.g. %2F should be %252F on filesystems. Something to do with decodeURI, but seems like only on the filename - not the whole path
-export const Thumbnail = observer(({ file, mounted, forceNoThumbnail, hovered, galleryVideoPlaybackMode }: ItemProps) => {
-  const { uiStore, imageLoader } = useStore();
-  const { thumbnailPath, isBroken } = file;
-
-  // This will check whether a thumbnail exists, generate it if needed
-  const imageSource = usePromise(
+export const Thumbnail = observer(
+  ({
     file,
-    isBroken,
     mounted,
-    thumbnailPath,
-    uiStore.isList || !forceNoThumbnail,
-    async (file, isBroken, mounted, thumbnailPath, useThumbnail) => {
-      // If it is broken, only show thumbnail if it exists.
-      if (!mounted || isBroken === true) {
-        if (await fse.pathExists(thumbnailPath)) {
-          return thumbnailPath;
-        } else {
-          throw new Error('No thumbnail available.');
-        }
-      }
+    forceNoThumbnail,
+    hovered,
+    galleryVideoPlaybackMode,
+    isSlideMode,
+  }: ItemProps) => {
+    const { uiStore, imageLoader } = useStore();
+    const { thumbnailPath, isBroken } = file;
 
-      if (useThumbnail) {
-        const freshlyGenerated = await imageLoader.ensureThumbnail(file);
-        // The thumbnailPath of an image is always set, but may not exist yet.
-        // When the thumbnail is finished generating, the path will be changed to `${thumbnailPath}?v=1`.
-        if (freshlyGenerated) {
-          await when(() => file.thumbnailPath.endsWith('?v=1'), { timeout: 10000 });
-          if (!getThumbnail(file).endsWith('?v=1')) {
-            throw new Error('Thumbnail generation timeout.');
+    // This will check whether a thumbnail exists, generate it if needed
+    const imageSource = usePromise(
+      file,
+      isBroken,
+      mounted,
+      thumbnailPath,
+      uiStore.isList || !forceNoThumbnail,
+      async (file, isBroken, mounted, thumbnailPath, useThumbnail) => {
+        // If it is broken, only show thumbnail if it exists.
+        if (!mounted || isBroken === true) {
+          if (await fse.pathExists(thumbnailPath)) {
+            return thumbnailPath;
+          } else {
+            throw new Error('No thumbnail available.');
           }
         }
-        return getThumbnail(file);
-      } else {
-        const src = await imageLoader.getImageSrc(file);
-        if (src !== undefined) {
-          return src;
+
+        if (useThumbnail) {
+          const freshlyGenerated = await imageLoader.ensureThumbnail(file);
+          // The thumbnailPath of an image is always set, but may not exist yet.
+          // When the thumbnail is finished generating, the path will be changed to `${thumbnailPath}?v=1`.
+          if (freshlyGenerated) {
+            await when(() => file.thumbnailPath.endsWith('?v=1'), { timeout: 10000 });
+            if (!getThumbnail(file).endsWith('?v=1')) {
+              throw new Error('Thumbnail generation timeout.');
+            }
+          }
+          return getThumbnail(file);
         } else {
-          throw new Error('No thumbnail available.');
+          const src = await imageLoader.getImageSrc(file);
+          if (src !== undefined) {
+            return src;
+          } else {
+            throw new Error('No thumbnail available.');
+          }
         }
+      },
+    );
+
+    // Even though all thumbnail errors should be caught in the above usePromise,
+    // there is a chance that the image cannot be loaded, and we don't want to show broken image icons
+    const fileId = file.id;
+    const fileIdRef = useRef(fileId);
+    const [loadError, setLoadError] = useState(false);
+    const handleImageError = useCallback(() => {
+      if (fileIdRef.current === fileId) {
+        setLoadError(true);
       }
-    },
-  );
+    }, [fileId]);
+    useEffect(() => {
+      fileIdRef.current = fileId;
+      setLoadError(false);
+    }, [fileId]);
 
-  // Even though all thumbnail errors should be caught in the above usePromise,
-  // there is a chance that the image cannot be loaded, and we don't want to show broken image icons
-  const fileId = file.id;
-  const fileIdRef = useRef(fileId);
-  const [loadError, setLoadError] = useState(false);
-  const handleImageError = useCallback(() => {
-    if (fileIdRef.current === fileId) {
-      setLoadError(true);
-    }
-  }, [fileId]);
-  useEffect(() => {
-    fileIdRef.current = fileId;
-    setLoadError(false);
-  }, [fileId]);
-
-  // Plays and pauses video
-  const thumbnailRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    if (thumbnailRef.current === null) {
-      return;
-    }
-    if (isFileExtensionVideo(file.extension)) {
+    // Plays and pauses video
+    const thumbnailRef = useRef<HTMLVideoElement>(null);
+    useEffect(() => {
+      if (thumbnailRef.current === null || !isFileExtensionVideo(file.extension)) {
+        return;
+      }
       if (hovered) {
         thumbnailRef.current.play();
       } else {
         thumbnailRef.current.pause();
         thumbnailRef.current.currentTime = 0;
       }
-    }
-  }, [thumbnailRef, hovered]);
-  useEffect(() => {
-    if (thumbnailRef.current !== null) {
-      if (isFileExtensionVideo(file.extension)) {
+    }, [thumbnailRef, hovered]);
+    useEffect(() => {
+      if (thumbnailRef.current === null || !isFileExtensionVideo(file.extension)) {
+        return;
+      }
+      if (galleryVideoPlaybackMode === 'auto') {
+        thumbnailRef.current.play();
+      } else {
+        thumbnailRef.current.pause();
+        thumbnailRef.current.currentTime = 0;
+      }
+    }, [thumbnailRef, galleryVideoPlaybackMode]);
+
+    // Pause video when slide mode, don't want to decode when video isn't visible
+    useEffect(() => {
+      if (thumbnailRef.current === null || !isFileExtensionVideo(file.extension)) {
+        return;
+      }
+      if (isSlideMode) {
+        thumbnailRef.current.pause();
+      } else {
         if (galleryVideoPlaybackMode === 'auto') {
           thumbnailRef.current.play();
-        } else {
-          thumbnailRef.current.pause();
-          thumbnailRef.current.currentTime = 0;
         }
       }
-    }
-  }, [thumbnailRef, galleryVideoPlaybackMode]);
+    }, [thumbnailRef, isSlideMode]);
 
-  if (!mounted) {
-    return <span className="image-placeholder" />;
-  } else if (loadError) {
-    return <span className="image-loading" />;
-  } else if (imageSource.tag === 'ready') {
-    if ('ok' in imageSource.value) {
-      const is_lowres = file.width < 320 || file.height < 320;
-      // TODO: add thumbnails to video for performance in gallery view
-      if (isFileExtensionVideo(file.extension)) {
-        const videoProps = {
-          src: encodeFilePath(imageSource.value.ok),
-          'data-file-id': file.id,
-          onError: handleImageError,
-          muted: true,
-          autoPlay: false,
-          loop: true,
-        };
-        if (galleryVideoPlaybackMode === 'auto' || hovered) {
-          videoProps.autoPlay = true;
-        }
-
-        return <video ref={thumbnailRef} {...videoProps} />;
-      }
-
-      return (
-        <img
-          src={encodeFilePath(imageSource.value.ok)}
-          alt=""
-          data-file-id={file.id}
-          onError={handleImageError}
-          style={
-            is_lowres && uiStore.upscaleMode == 'pixelated' ? { imageRendering: 'pixelated' } : {}
+    if (!mounted) {
+      return <span className="image-placeholder" />;
+    } else if (loadError) {
+      return <span className="image-loading" />;
+    } else if (imageSource.tag === 'ready') {
+      if ('ok' in imageSource.value) {
+        const is_lowres = file.width < 320 || file.height < 320;
+        // TODO: add thumbnails to video for performance in gallery view
+        if (isFileExtensionVideo(file.extension)) {
+          const videoProps = {
+            src: encodeFilePath(imageSource.value.ok),
+            'data-file-id': file.id,
+            onError: handleImageError,
+            muted: true,
+            autoPlay: false,
+            loop: true,
+          };
+          if (galleryVideoPlaybackMode === 'auto' || hovered) {
+            videoProps.autoPlay = true;
           }
-        />
-      );
+
+          return <video ref={thumbnailRef} {...videoProps} />;
+        }
+
+        return (
+          <img
+            src={encodeFilePath(imageSource.value.ok)}
+            alt=""
+            data-file-id={file.id}
+            onError={handleImageError}
+            style={
+              is_lowres && uiStore.upscaleMode == 'pixelated' ? { imageRendering: 'pixelated' } : {}
+            }
+          />
+        );
+      } else {
+        return <span className="image-error" />;
+      }
     } else {
-      return <span className="image-error" />;
+      return <span className="image-loading" />;
     }
-  } else {
-    return <span className="image-loading" />;
-  }
-});
+  },
+);
 
 const getThumbnail = action((file: ClientFile) => file.thumbnailPath);
 
