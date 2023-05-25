bc9c90c697df7136045d145a40e51de14ad77a4b
hummingly <31522351+hummingly@users.noreply.github.com>
2023-05-25 11:20:49 +0200

    Use method syntax in DataStorage interface *It is probably better for readability but mostly better IDE generation of methods.

--- src/api/data-storage.ts
+++ src/api/data-storage.ts
@@ -16,34 +16,34 @@ import { TagDTO } from './tag';
  * * searches (list).
  */
 export interface DataStorage {
-  fetchTags: () => Promise<TagDTO[]>;
-  fetchFiles: (order: OrderBy<FileDTO>, fileOrder: OrderDirection) => Promise<FileDTO[]>;
-  fetchFilesByID: (ids: ID[]) => Promise<FileDTO[]>;
-  fetchFilesByKey: (key: keyof FileDTO, value: IndexableType) => Promise<FileDTO[]>;
-  fetchLocations: () => Promise<LocationDTO[]>;
-  fetchSearches: () => Promise<FileSearchDTO[]>;
-  searchFiles: (
+  fetchTags(): Promise<TagDTO[]>;
+  fetchFiles(order: OrderBy<FileDTO>, fileOrder: OrderDirection): Promise<FileDTO[]>;
+  fetchFilesByID(ids: ID[]): Promise<FileDTO[]>;
+  fetchFilesByKey(key: keyof FileDTO, value: IndexableType): Promise<FileDTO[]>;
+  fetchLocations(): Promise<LocationDTO[]>;
+  fetchSearches(): Promise<FileSearchDTO[]>;
+  searchFiles(
     criteria: ConditionDTO<FileDTO> | [ConditionDTO<FileDTO>, ...ConditionDTO<FileDTO>[]],
     order: OrderBy<FileDTO>,
     fileOrder: OrderDirection,
     matchAny?: boolean,
-  ) => Promise<FileDTO[]>;
-  createTag: (tag: TagDTO) => Promise<void>;
-  createFilesFromPath: (path: string, files: FileDTO[]) => Promise<void>;
-  createLocation: (location: LocationDTO) => Promise<void>;
-  createSearch: (search: FileSearchDTO) => Promise<void>;
-  saveTag: (tag: TagDTO) => Promise<void>;
-  saveFiles: (files: FileDTO[]) => Promise<void>;
-  saveLocation: (location: LocationDTO) => Promise<void>;
-  saveSearch: (search: FileSearchDTO) => Promise<void>;
-  removeTags: (tags: ID[]) => Promise<void>;
-  mergeTags: (tagToBeRemoved: ID, tagToMergeWith: ID) => Promise<void>;
-  removeFiles: (files: ID[]) => Promise<void>;
-  removeLocation: (location: ID) => Promise<void>;
-  removeSearch: (search: ID) => Promise<void>;
-  countFiles: () => Promise<[fileCount: number, untaggedFileCount: number]>;
-  clear: () => Promise<void>;
-  backupToFile: (path: string) => Promise<void>;
-  restoreFromFile: (path: string) => Promise<void>;
-  peekFile: (path: string) => Promise<[numTags: number, numFiles: number]>;
+  ): Promise<FileDTO[]>;
+  createTag(tag: TagDTO): Promise<void>;
+  createFilesFromPath(path: string, files: FileDTO[]): Promise<void>;
+  createLocation(location: LocationDTO): Promise<void>;
+  createSearch(search: FileSearchDTO): Promise<void>;
+  saveTag(tag: TagDTO): Promise<void>;
+  saveFiles(files: FileDTO[]): Promise<void>;
+  saveLocation(location: LocationDTO): Promise<void>;
+  saveSearch(search: FileSearchDTO): Promise<void>;
+  removeTags(tags: ID[]): Promise<void>;
+  mergeTags(tagToBeRemoved: ID, tagToMergeWith: ID): Promise<void>;
+  removeFiles(files: ID[]): Promise<void>;
+  removeLocation(location: ID): Promise<void>;
+  removeSearch(search: ID): Promise<void>;
+  countFiles(): Promise<[fileCount: number, untaggedFileCount: number]>;
+  clear(): Promise<void>;
+  backupToFile(path: string): Promise<void>;
+  restoreFromFile(path: string): Promise<void>;
+  peekFile(path: string): Promise<[numTags: number, numFiles: number]>;
 }
